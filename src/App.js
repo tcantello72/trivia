519cfa02cbb85c33c38acd6cc15f551b4d099d7c
toby.cantello@yum.com <toby.cantello@yum.com>
2023-07-05 15:54:41 -0500

    finished project
    finished project

--- src/App.js
+++ src/App.js
@@ -4,24 +4,47 @@ import { Question } from './components';
 const category = '';
 const TRIVIA_API = `https://opentdb.com/api.php?amount=1&category=${category}&difficulty=easy`;
 
+
 class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = { question: null }
+    }
 
+    componentDidMount() {
+        this.fetchTriviaQuestion();
+    }
 
-  render() {
-    return (
-      <div className='container l:w-50 p-5'>
-        <h1 className='display-1'>Trivia</h1>
-        <h2 className='fw-lighter fs-5 mb-4'>
-          (we couldn&lsquo;t think of a better name,{' '}
-          <span className='fw-bolder'>sorry</span>)
-        </h2>
-        <hr />
-        <div>
-          {/* Render question here */}
-        </div>
-      </div>
-    );
-  }
+    fetchTriviaQuestion = () => {
+      fetch(TRIVIA_API)
+          .then((res) => res.json())
+          .then((data) => {
+              console.log(data);
+              const question = data.results[0];
+              this.setState({ question })
+          })
+          .catch((err) => {
+              console.error("Error fetching trivia question:", err);
+          })
+    };
+
+    render() {
+        const { question } = this.state;
+
+        return (
+            <div className='container l:w-50 p-5'>
+                <h1 className='display-1'>Trivia</h1>
+                <h2 className='fw-lighter fs-5 mb-4'>
+                    (we couldn&lsquo;t think of a better name,{' '}
+                    <span className='fw-bolder'>sorry</span>)
+                </h2>
+                <hr />
+                <div>
+                    {this.state.question && <Question question={this.state.question}/>}
+                </div>
+            </div>
+        );
+    }
 }
 
 export { App };
--- src/components/Question.js
+++ src/components/Question.js
@@ -3,46 +3,62 @@ import { AnswerButton } from './';
 import { decodeHTML, randomizeArray } from '../lib';
 
 class Question extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      guessed: false,
-      guess: '',
+    constructor(props) {
+      super(props);
+      this.state = {
+        guessed: false,
+        guess: '',
+      };
+
+      // convert all answers into a single array, and randomize the array
+      this.answers = randomizeArray([
+        ...props.question.incorrect_answers,
+        props.question.correct_answer,
+      ]);
+    }
+      
+    handleGuess = (answer) => {
+      // set guessed to true, and set guess to the selected answer
+      this.setState({ guessed: true, guess: answer });
     };
 
-    // convert all answers into a single array, and randomize the array
-    this.answers = randomizeArray([
-      ...props.question.incorrect_answers,
-      props.question.correct_answer,
-    ]);
-  }
+    render() {
+      
 
-  handleGuess = (answer) => {
-    // set guessed to true, and set guess to the selected answer
-    this.setState({ guessed: true, guess: answer });
-  };
+      return (
+        <div className='card p-2 mb-4'>
+          <h3 className='fw-lighter fs-5 mb-4'>{this.props.question.category}</h3>
+          <h4 className='fw-light fs-5 mb-4'>
+            {decodeHTML(this.props.question.question)}
+          </h4>
 
-  render() {
-    return (
-      <div className='card p-2 mb-4'>
-        <h3 className='fw-lighter fs-5 mb-4'>{this.props.question.category}</h3>
-        <h4 className='fw-light fs-5 mb-4'>
-          {decodeHTML(this.props.question.question)}
-        </h4>
+          <div>
+            {this.answers.map((answer) => (
+              <AnswerButton
+                key={answer}
+                answer={answer}
+                handleGuess={() => this.handleGuess(answer)}
+              />
+            ))}
+          </div>
 
-        <div>
-          {this.answers.map((answer) => (
-            <AnswerButton
-              key={answer}
-              answer={answer}
-            />
-          ))}
+      {/* Dynamically render correct/incorrect here! */}
+      {this.state.guessed && (
+          <div>
+            {this.state.guess === this.props.question.correct_answer ? (
+              <p>Correct!</p>
+            ) : (
+              <p>
+                Incorrect! The correct answer is{" "}
+                {`${this.props.question.correct_answer}`}
+              </p>
+            )}
+          </div>
+        )}
         </div>
-
-        {/* Dynamically render correct/incorrect here! */}
-      </div>
-    );
-  }
+      );
+    }
 }
+
 
 export { Question };
